370d208c31c9418ccd36af24e38adcb2b9fa4748
AngryKat <gatto_candido@ukr.net>
2025-01-25 00:56:19 +0200

    Create buttons for store actions

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
-import {Pressable} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useDispatch, useSelector} from 'react-redux';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import {
+  decrementLikes,
+  incrementLikes,
+  selectIsCharacterLiked,
+} from '../store/fansSlice';
+import {Gender} from '../types';
+import {Pressable} from 'react-native';
 
 type Props = {
   accessibilityLabel: string;
-  isLiked: boolean;
-  onPress: () => void;
+  gender: Gender;
+  characterUrl: string;
 };
 
-export function LikeButton({accessibilityLabel, isLiked, onPress}: Props) {
+export function LikeButton({accessibilityLabel, characterUrl, gender}: Props) {
+  const scale = useSharedValue(1);
+  const isLiked = useSelector(selectIsCharacterLiked(gender, characterUrl));
+  const dispatch = useDispatch();
+  const handleLikePress = () => {
+    if (isLiked) {
+      dispatch(decrementLikes({gender, characterUrl}));
+    } else {
+      dispatch(incrementLikes({gender, characterUrl}));
+    }
+  };
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value}],
+  }));
+
   return (
     <Pressable
       accessibilityLabel={accessibilityLabel}
       accessibilityRole="button"
-      onPress={onPress}>
-      <Icon
-        name={isLiked ? 'heart' : 'heart-outline'}
-        size={30}
-        color="rgba(255, 42, 36, 0.78)"
-      />
+      onPress={handleLikePress}
+      style={animatedStyle}
+      onPressIn={() => {
+        scale.value = withSpring(1.2);
+      }}
+      onPressOut={() => {
+        scale.value = withSpring(1);
+      }}>
+      <Animated.View style={animatedStyle}>
+        <Icon
+          name={isLiked ? 'heart' : 'heart-outline'}
+          size={30}
+          color="rgba(255, 42, 36, 0.78)"
+        />
+      </Animated.View>
     </Pressable>
   );
 }
