dd46b93bffa804b7ec0fc2d3b35024806f83c889
AngryKat <gatto_candido@ukr.net>
2025-01-25 00:55:35 +0200

    Set up redux

---
+++ src/store/fansSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { Gender } from '../types';
+import { RootState } from '.';
+
+export interface FansState {
+  female: { count: number; characterUrls: Record<string, string> };
+  male: { count: number; characterUrls: Record<string, string> };
+  other: { count: number; characterUrls: Record<string, string> };
+}
+
+const initialState: FansState = {
+  female: { count: 0, characterUrls: {} },
+  male: { count: 0, characterUrls: {} },
+  other: { count: 0, characterUrls: {} },
+};
+
+export const fansSlice = createSlice({
+  name: 'fans',
+  initialState,
+  reducers: {
+    incrementLikes: (
+      state,
+      action: PayloadAction<{ gender: Gender; characterUrl: string }>,
+    ) => {
+      const { gender, characterUrl } = action.payload;
+      state[gender].count += 1;
+      state[gender].characterUrls[characterUrl] = characterUrl;
+    },
+    decrementLikes: (
+      state,
+      action: PayloadAction<{ gender: Gender; characterUrl: string }>,
+    ) => {
+      const { gender, characterUrl } = action.payload;
+      state[gender].count -= 1;
+      delete state[gender].characterUrls[characterUrl];
+    },
+    reset: () => initialState,
+  },
+});
+
+export const { reset, decrementLikes, incrementLikes } = fansSlice.actions;
+
+export const selectFansCount = (gender: Gender) => (state: RootState) =>
+  state.fans[gender].count;
+export const selectIsCharacterLiked =
+  (gender: Gender, characterUrl: string) => (state: RootState) =>
+    !!state.fans[gender]?.characterUrls[characterUrl];
+
+export default fansSlice.reducer;
